5e5ee9a35c4425c80ded8530d3434128f7d6dac9
KMV <rowm30@gmail.com>
2020-11-14 01:30:30 +0530

    LL_Q3_Incomplete

---
+++ LinkedList/Q3.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+import java.util.*;
+
+public class Q3 {
+    public static void main(String[] args){
+        Map<String, Integer> numbers = new HashMap<>();
+
+        numbers.put("one", 1);
+        numbers.put("three", 3);
+        numbers.put("Mayank", 786);
+        numbers.put("God", 1);
+        System.out.println(numbers);
+    }
+}
