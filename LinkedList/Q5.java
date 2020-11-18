3cf9c2a4985eb1a4a900dd69b976639146bd23a3
KMV <rowm30@gmail.com>
2020-11-19 01:08:00 +0530

    LL_Q5_InComplete

--- LinkedList/Q5.java
+++ LinkedList/Q5.java
@@ -1,5 +1,31 @@
 package LinkedList;
 
 public class Q5 {
+
+    private Node head;
+
+    private static class Node {
+        private int value;
+        private Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    public void addToTheLast(Node node){
+        if(head == null){
+            head = node;
+        }else{
+            Node temp = head;
+            while (temp.next  != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
     
+    public void printList(){
+        Node temp = head;
+    }
 }
