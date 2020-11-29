c1fea921c84769231ff77be3bcc8c3fe9c3c6318
KMV <rowm30@gmail.com>
2020-11-29 20:42:59 +0530

    Working

--- BinaryTree/Q4.java
+++ BinaryTree/Q4.java
@@ -3,7 +3,22 @@ package BinaryTree;
 
 
 public class Q4 {
-    
+    Node root;
+
+    int diameter(Node root){
+        if (root == null){
+            return 0;
+        }
+
+        int lheight = height(root.left);
+        int rheight = hieght(root.right);
+
+        int ldiameter = diameter(root.left);
+        int rdiameter = diameter(root.left);
+
+        return Math.max(lheight + rheight +1, Math.max(ldiameter, rdiameter));
+
+    }
 }
 
 class Node {
@@ -11,6 +26,7 @@ class Node {
     Node left,right;
 
     public Node(int item){
-        
+        data= item;
+        left = right = null;
     }
 }
