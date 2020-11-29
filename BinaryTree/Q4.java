f85aa33ee6ab23bda4a94a2bd9470caf2b2d4f47
KMV <rowm30@gmail.com>
2020-11-29 20:04:05 +0530

    Rebase

---
+++ BinaryTree/Q4.java
@@ -0,0 +1,22 @@
+package BinaryTree;
+
+
+
+public class Q4 {
+    Node root;
+
+    int diameter(Node root){
+        
+        //Base case if tree is empty 
+    }
+}
+
+class Node {
+    int data;
+    Node left,right;
+
+    public Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
---
+++ LinkedList/Q5.java
@@ -0,0 +1,79 @@
+package LinkedList;
+
+public class Q5 {
+
+    private Node head;
+
+    private static class Node {
+        private int value;
+        private Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    public void addToTheLast(Node node){
+        if(head == null){
+            head = node;
+        }else{
+            Node temp = head;
+            while (temp.next  != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+    
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public Node findStartNodeOfTheLoop(){
+        Node fastPtr = head;
+        Node slowPtr = head;
+        boolean loopExist = false;
+        while(fastPtr != null && fastPtr.next != null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr == fastPtr) {
+                loopExist = true;
+                break;
+            }
+        }
+        if (loopExist) {
+            slowPtr = head;
+            while(slowPtr != fastPtr){
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next;
+            }
+        } else {
+            System.out.println("Loop dost not exists");
+            slowPtr = null;
+        }
+        return slowPtr;
+    }
+
+    public static void main(String[] args){
+        Q5 list = new Q5();
+        Node loopNode = new Node(7);
+        list.addToTheLast(new Node(5));
+        list.addToTheLast(new Node(6));
+        list.addToTheLast(loopNode);
+        list.addToTheLast(new Node(1));
+        list.addToTheLast(new Node(2));
+
+        list.printList();
+        list.addToTheLast(loopNode);
+
+        Node startNode = list.findStartNodeOfTheLoop();
+        if (startNode!=null) {
+            System.out.println("start Node of loop is " + startNode.value);
+        }
+        // Final
+    }
+}
