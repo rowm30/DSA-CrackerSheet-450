bb3015476b77e03a046e56735a4f175ee222bbb9
KMV <rowm30@gmail.com>
2020-12-05 21:06:18 +0530

    ResumeWork

--- BinaryTree/Q4.java
+++ BinaryTree/Q4.java
@@ -1,7 +1,24 @@
 package BinaryTree;
 
+import org.graalvm.compiler.nodes.extended.LoadHubNode;
+
 class Q4{
     Node root;
+    
+    int diameter(Node root){
+        if (root == null){
+            return 0;
+        }
+
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+
+        int ldiameter = diameter(root.left);
+        int rdiameter = diameter(root.right);
+
+        return Math.max(lheight + rheight + 1, Math.max(ldiameter, rdiameter));
+    }
+
     
 }
 
