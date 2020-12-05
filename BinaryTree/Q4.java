36d2e47ee6cb29113f8aaf7ad90ea48e90be6a2e
KMV <rowm30@gmail.com>
2020-12-05 18:17:29 +0530

    ResumeWork

--- BinaryTree/Q4.java
+++ BinaryTree/Q4.java
@@ -1,2 +1,16 @@
 package BinaryTree;
 
+class Q4{
+    Node root;
+    
+}
+
+class Node{
+    int data;
+    Node left,right;
+
+    public Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
