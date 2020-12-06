d1e7a9dff26f0d220f0d94db7f9dedb9b0744ee5
KMV <rowm30@gmail.com>
2020-12-06 14:13:52 +0530

    BT_Q4_Complete

--- BinaryTree/Q4.java
+++ BinaryTree/Q4.java
@@ -36,6 +36,10 @@ class Q4 {
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
         tree.root.left.right = new Node(5);
+        tree.root.right.right = new Node(5);
+        tree.root.right.left = new Node(5);
+        tree.root.right.left.right = new Node(5);
+        tree.root.right.left.left = new Node(5);
         
         System.out.println("The diameter of given binary tree is : " + tree.diameter());
     }
