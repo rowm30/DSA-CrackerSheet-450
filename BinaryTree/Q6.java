1135a46aad1cfce771dd3f84d4567c7e3915ca07
KMV <rowm30@gmail.com>
2020-12-11 21:44:59 +0530

    Resume2

--- BinaryTree/Q6.java
+++ BinaryTree/Q6.java
@@ -1,5 +1,19 @@
 package BinaryTree;
 
+import java.util.*;
+
 public class Q6 {
     
+}
+
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
