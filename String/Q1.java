90b4688f4ff32fe25cefce35376544ce83ee2dc5
KMV <rowm30@gmail.com>
2020-12-25 17:57:24 +0530

    Day2

--- String/Q1.java
+++ String/Q1.java
@@ -6,7 +6,13 @@ public class Q1 {
     
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        String input = sc.next();
     }
+
+    public void reverseString(char[] s){
+
+    }
+
+    
 
 }
