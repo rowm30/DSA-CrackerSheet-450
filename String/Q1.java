3914111927073505a2a1f76ec634fb87d5a7a371
KMV <rowm30@gmail.com>
2020-12-25 17:54:14 +0530

    Day2

--- String/Q1.java
+++ String/Q1.java
@@ -1,7 +1,12 @@
 package String;
 
-import
+import java.util.*;
 
 public class Q1 {
     
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+
 }
