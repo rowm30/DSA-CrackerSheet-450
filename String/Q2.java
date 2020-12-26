2cd3119d22ad4df3ec994d81ce103fa63007aedc
KMV <rowm30@gmail.com>
2020-12-26 13:10:39 +0530

    String_Q2_InComplete

--- String/Q2.java
+++ String/Q2.java
@@ -1,8 +1,33 @@
 package String;
 
+import java.util.*;
+
 public class Q2 {
     public static boolean isPalindrome(String str){
         int i = 0, j = str.length() - 1;
         
+        while (i < j){
+
+            if ( str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        if(isPalindrome(str)){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
     }
 }
