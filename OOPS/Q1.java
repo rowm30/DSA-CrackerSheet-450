12aa27433053e52a7e52490ce2330e490429670b
KMV <rowm30@gmail.com>
2021-01-02 13:20:18 +0530

    2021

--- OOPS/Q1.java
+++ OOPS/Q1.java
@@ -1,5 +1,18 @@
 package OOPS;
 
 public class Q1 {
+    // Instance Variables
+    String name;
+    String breed;
+    int age;
+    String color;
+
+    // Constructor Declaration of Class
+    public Q1(String name,String breed,int age, String color){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
     
 }
