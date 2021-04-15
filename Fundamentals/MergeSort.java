f25d2fad6cbacc11384c80adfb0ca3ce359fff7d
rowm30 <rowm30@gmail.com>
2021-04-16 01:24:35 +0530

    B_Consistent_Day-3

--- Fundamentals/MergeSort.java
+++ Fundamentals/MergeSort.java
@@ -2,21 +2,30 @@ package Fundamentals;
 
 import java.util.*;
 
-public class MergeSort {
-    
-    public static void merge(int arr[], int l, int m, int r){
+public class MergeSort{
+
+    public static void merge(int[] arr, int l, int m, int r){
+
+        // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        int L[] = new int[n1];
-        int R[] = new int[n2];
+        // Create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
 
-        for (int i = 0; i < n1; ++i) {
+        // Copy data to temp array
+        for (int i = 0; i < n1; i++) {
             L[i] = arr[l + i];
         }
-
-        for (int i = 0; i < n2; ++i) {
+        for (int i = 0; i < n2; i++) {
             R[i] = arr[m + 1 + i];
         }
+
+        // Merge the temp arrays
+
+        int i = 0, j = 0;
+
+        
     }
 }
