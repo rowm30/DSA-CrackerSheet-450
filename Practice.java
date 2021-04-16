4d8600afef5cea284219cd68579753d16be32aa7
rowm30 <rowm30@gmail.com>
2021-04-16 05:51:48 +0530

    QuickSort_Start

--- Practice.java
+++ Practice.java
@@ -1,98 +1,90 @@
-/* Java program for Merge Sort */
-class MergeSort
+// Java implementation of QuickSort
+import java.io.*;
+
+class GFG{
+	
+// A utility function to swap two elements
+static void swap(int[] arr, int i, int j)
 {
-	// Merges two subarrays of arr[].
-	// First subarray is arr[l..m]
-	// Second subarray is arr[m+1..r]
-	void merge(int arr[], int l, int m, int r)
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+static int partition(int[] arr, int low, int high)
+{
+	
+	// pivot
+	int pivot = arr[high];
+	
+	// Index of smaller element and
+	// indicates the right position
+	// of pivot found so far
+	int i = (low - 1);
+
+	for(int j = low; j <= high - 1; j++)
 	{
-		// Find sizes of two subarrays to be merged
-		int n1 = m - l + 1;
-		int n2 = r - m;
-
-		/* Create temp arrays */
-		int L[] = new int[n1];
-		int R[] = new int[n2];
-
-		/*Copy data to temp arrays*/
-		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
-
-		/* Merge the temp arrays */
-
-		// Initial indexes of first and second subarrays
-		int i = 0, j = 0;
-
-		// Initial index of merged subarry array
-		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			}
-			else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
-
-		/* Copy remaining elements of L[] if any */
-		while (i < n1) {
-			arr[k] = L[i];
+		
+		// If current element is smaller
+		// than the pivot
+		if (arr[j] < pivot)
+		{
+			
+			// Increment index of
+			// smaller element
 			i++;
-			k++;
-		}
-
-		/* Copy remaining elements of R[] if any */
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
+			swap(arr, i, j);
 		}
 	}
+	swap(arr, i + 1, high);
+	return (i + 1);
+}
 
-	// Main function that sorts arr[l..r] using
-	// merge()
-	void sort(int arr[], int l, int r)
+/* The main function that implements QuickSort
+		arr[] --> Array to be sorted,
+		low --> Starting index,
+		high --> Ending index
+*/
+static void quickSort(int[] arr, int low, int high)
+{
+	if (low < high)
 	{
-		if (l < r) {
-			// Find the middle point
-			int m =l+ (r-l)/2;
+		
+		// pi is partitioning index, arr[p]
+		// is now at right place
+		int pi = partition(arr, low, high);
 
-			// Sort first and second halves
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
-
-			// Merge the sorted halves
-			merge(arr, l, m, r);
-		}
-	}
-
-	/* A utility function to print array of size n */
-	static void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-
-	// Driver code
-	public static void main(String args[])
-	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-		System.out.println("Given Array");
-		printArray(arr);
-
-		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
-
-		System.out.println("\nSorted array");
-		printArray(arr);
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
 	}
 }
-/* This code is contributed by Rajat Mishra */
+
+// Function to print an array
+static void printArray(int[] arr, int size)
+{
+	for(int i = 0; i < size; i++)
+		System.out.print(arr[i] + " ");
+		
+	System.out.println();
+}
+
+// Driver Code
+public static void main(String[] args)
+{
+	int[] arr = { 10, 7, 8, 9, 1, 5 };
+	int n = arr.length;
+	
+	quickSort(arr, 0, n - 1);
+	System.out.println("Sorted array: ");
+	printArray(arr, n);
+}
+}
+
+// This code is contributed by Ayush Choudhary
