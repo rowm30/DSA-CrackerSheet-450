7b61fbd110522186aafd6cb5649859b4aab6ae6a
rowm30 <rowm30@gmail.com>
2021-05-15 09:55:01 +0530

    DSA-Forever

--- Practice.java
+++ Practice.java
@@ -1,40 +1,58 @@
-// Java program to put all negative
-// numbers before positive numbers
-import java.io.*;
+package LinkedList;
 
-class GFG {
+// Java program for reversing the Linked lsit
 
-	static void rearrange(int arr[], int n)
-	{
-		int j = 0, temp;
-		for (int i = 0; i < n; i++) {
-			if (arr[i] < 0) {
-				if (i != j) {
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-				j++;
-			}
-		}
-	}
+class LinkedList{
+    static Node head;
 
-	// A utility function to print an array
-	static void printArray(int arr[], int n)
-	{
-		for (int i = 0; i < n; i++)
-			System.out.print(arr[i] + " ");
-	}
+    static class Node{
+        int data;
+        Node next;
 
-	// Driver code
-	public static void main(String args[])
-	{
-		int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
-		int n = arr.length;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+    // Function to reverse the linked list
+    Node reverse(Node node){
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
 
-		rearrange(arr, n);
-		printArray(arr, n);
+    // Print content of double linked list
+    void printList(Node node){
+        while(node != null){
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.head = new Node(85);
+        list.head.next = new Node(15);
+        list.head.next.next = new Node(4);
+        list.head.next.next.next = new Node(20);
+
+        System.out.println("Given Linked list");
+        list.printList(head);
+        head = list.reverse(head);
+        System.out.println("");
+        System.out.println("Reversed linked list");
+        list.printList(head);
+    }
+
+	public void push(int data) {
 	}
 }
-
-// This code is contributed by Nikita Tiwari.
