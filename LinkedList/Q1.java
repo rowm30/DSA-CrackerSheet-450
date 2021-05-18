49fcba9ea1db564de06f486f21bcbcfd0309ae7a
rowm30 <rowm30@gmail.com>
2021-05-19 05:14:54 +0530

    Fresh_Perspective

--- LinkedList/Q1.java
+++ LinkedList/Q1.java
@@ -1,11 +1,33 @@
 package LinkedList;
 
-class LinkedList{
+class Q1{
 
     static Node head;
 
     static class Node{
 
         int data;
+        Node next;
+
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    // Reverse the List
+    Node reverse(Node node){
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
     }
 }
