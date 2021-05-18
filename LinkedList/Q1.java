6f88b9635cef41b5aa5ee663ada4a55206ee97b8
rowm30 <rowm30@gmail.com>
2021-05-19 05:25:07 +0530

    Fresh_Perspective

--- LinkedList/Q1.java
+++ LinkedList/Q1.java
@@ -30,4 +30,30 @@ class Q1{
         node = prev;
         return node;
     }
+
+    // Print content of double linked list
+    public static void printList(Node node){
+        while(node != null){
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args){
+        Q1 list = new Q1();
+        list.head = new Node(42);
+        list.head.next = new Node(12);
+        list.head.next.next = new Node(21);
+        list.head.next.next.next = new Node(66);
+        list.head.next.next.next.next = new Node(90);
+
+        System.out.println("Given Linked list:");
+        list.printList(head);
+        head = list.reverse(head);
+        System.out.println("");
+        System.out.println("Reverse linked list");
+        list.printList(head);
+    }
+
+
 }
