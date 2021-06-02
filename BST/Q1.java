26c3fdf1afdd582f7fd4840ded2564a2e69e266b
rowm30 <rowm30@gmail.com>
2021-06-02 16:37:06 +0530

    b_consistent

--- BST/Q1.java
+++ BST/Q1.java
@@ -1,7 +1,28 @@
 package LinkedList;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Q1 {
+    
+    class Node{
+        int key;
+        Node left,right;
+
+        Node(int item){
+            key = item;
+            left = right = null;
+        }
+    }
+
+
+    // Root of BST
+
+    Node root;
+
+    // Constructor
+    Q1(){
+        root = null;
+    }
+
     
 }
