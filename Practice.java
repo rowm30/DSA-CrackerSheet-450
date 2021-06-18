91fb79059df518616be2a10af25abf855a4fe157
rowm30 <rowm30@gmail.com>
2021-06-18 19:51:29 +0530

    Mqayan

--- Practice.java
+++ Practice.java
@@ -1,6 +1,13 @@
-class Practice{
-    public static void main(String[] args){
-        int[][] arr = new int[3][3];
-        arr = [1,2,3,4,5,6,7,8,9];
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int res = 0;
+        for(int i =0;i<accounts.length;i++){
+            int temp = 0;
+            for(int j = 0;j<accounts[i].length;j++){
+                temp+=accounts[i][j];
+            }
+            res = Math.max(res,temp);
+        }
+        return res;
     }
 }
