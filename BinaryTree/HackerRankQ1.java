58e51a8dc682a4360d9e72326cb2ecb8a679a9db
rowm30 <rowm30@gmail.com>
2021-06-21 22:01:42 +0530

    Tree

--- BinaryTree/HackerRankQ1.java
+++ BinaryTree/HackerRankQ1.java
@@ -1,3 +1,46 @@
+package BinaryTree;
+
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
 public class HackerRankQ1 {
     
+
+    public static Node insert(Node root, int data){
+        if(root == null){
+            return new Node(data);
+        }else{
+            Node cur;
+            if(data <= root.data){
+                cur = insert(root.left, data);
+                root.left = cur;
+            }else{
+                cur = insert(root.right,data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        Node root = null;
+        while(t-- > 0){
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        sc.close();
+    }
 }
