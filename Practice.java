9fd465d3b67b1d9392644a3a5499cc4d4ebb1c86
rowm30 <rowm30@gmail.com>
2021-07-27 04:58:41 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -46,20 +46,7 @@ class Solution {
             return root;
         }
     }
-      public static Node insert(Node root, int data) {
-        if (root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if (data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
+      
     }
     // WELCOME10
 
