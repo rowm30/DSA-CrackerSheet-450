ba85e4aebca8840884f705d031d2e9e08313f94a
rowm30 <rowm30@gmail.com>
2021-07-29 07:59:31 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -48,17 +48,5 @@ class Solution {
     }
       
     }
-    // WELCOME10
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        Node root = null;
-        while (t-- > 0) {
-            int data = scan.nextInt();
-            root = insert(root, data);
-        }
-        scan.close();
-        preOrder(root);
-    }
+    
 }
