3f495f68ccc251773b48d35b68e326cf6674c110
rowm30 <rowm30@gmail.com>
2021-07-30 10:56:13 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -31,22 +31,6 @@ class Solution {
         preOrder(root.right);
     }
 
-    public static Node insert(Node root, int data) {
-        if (root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if (data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
-      
-    }
+    
     
 }
