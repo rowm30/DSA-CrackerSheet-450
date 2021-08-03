8e69ca14c233999c80352d85c371421b535fb063
rowm30 <rowm30@gmail.com>
2021-08-03 08:45:44 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -3,22 +3,7 @@
 class Q3{
     NodeXL root;
 
-    int maxDepth(NodeXL node){
-        if (node == null) {
-            return 0;
-        } else {
-            // Compute the depth of each sub tree
-            int lDepth = maxDepth(node.left);
-            int rDepth = maxDepth(node.right);
-
-            // Use the larger one 
-            if (lDepth>rDepth) {
-                return(lDepth+1);
-            } else {
-                return(rDepth+1);
-            }
-        }
-    }
+    
 
     public static void main(String[] args){
         Q3 tree = new Q3();
