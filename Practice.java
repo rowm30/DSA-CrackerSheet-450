1492a18db1575f643cc4acdc58a44ad11364e4b6
rowm30 <rowm30@gmail.com>
2021-08-04 07:54:50 +0530

    mayanjk

--- Practice.java
+++ Practice.java
@@ -5,26 +5,6 @@ class Q3{
 
     
 
-    public static void main(String[] args){
-        Q3 tree = new Q3();
-
-        tree.root = new NodeXL(1);
-        tree.root.left = new NodeXL(2);
-        tree.root.right = new NodeXL(3);
-        tree.root.left.left = new NodeXL(4);
-        tree.root.left.right = new NodeXL(5);
-        tree.root.left.right.left = new NodeXL(5);
-        tree.root.left.right.right = new NodeXL(5);
-        tree.root.left.left.right = new NodeXL(5);
-        tree.root.left.left.right = new NodeXL(5);
-        tree.root.right.left = new NodeXL(5);
-        tree.root.right.right = new NodeXL(5);
-        tree.root.right.right.left = new NodeXL(5);
-        tree.root.right.right.left.right = new NodeXL(5);
-        
-        System.out.println("Height is : " + tree.maxDepth(tree.root));
-    }
-}
 
 class NodeXL{
     int data;
