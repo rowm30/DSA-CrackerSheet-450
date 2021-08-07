fcf632ad16da1a92a10616caa31db6bdb573624b
rowm30 <rowm30@gmail.com>
2021-08-07 14:15:06 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -11,9 +11,104 @@ public class Q2 {
         TreeNode(int data){
             this.data = data;
         }
+
     }
 
-    // print in revesal level order\
+    package Fundamentals;
+
+import java.util.*;
+
+public class MergeSort{
+
+    public static void merge(int[] arr, int l, int m, int r){
+
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temp array
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            R[i] = arr[m + 1 + i];
+        }
+
+        // Merge the temp arrays
+
+        int i = 0, j = 0;
+
+        // Initial index of merged sub array
+        int k = l;
+        while(i < n1 && j < n2){
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // copy remaining elements 
+
+        while(i < n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void sort(int arr[], int l, int r){
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r-l)/2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m+1, r);
+
+            // Merge the sorted halves
+            merge(arr,l , m,  r);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sort(arr, 0, n-1);
+
+        printArray(arr);
+
+        sc.close();
+    }
+}
+
+
+    // print in revesal level order\sdfklahdsfijkhadsjkfhadjksbgfahjkldsbfghjadsbvfhjabdsfabvdjklshfbvadhjsfbvadhjs
     public static void reverseLOT(TreeNode startNode){
         Queue<TreeNode> queue = new LinkedList<>();
         Stack<TreeNode> stack = new Stack<>();
