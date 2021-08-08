6310c2eaba59f4824d64e41282b583ba0166a883
rowm30 <rowm30@gmail.com>
2021-08-08 21:53:52 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -90,21 +90,7 @@ public class MergeSort{
         }
     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        sort(arr, 0, n-1);
-
-        printArray(arr);
-
-        sc.close();
-    }
+    
 }
 
 
