6ec80496695d53c4afd51d27cb7d8c51fb756573
rowm30 <rowm30@gmail.com>
2021-09-01 14:24:13 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -1,349 +1,130 @@
-class ParkingSystem {
-    int big, medium, small;
-    public ParkingSystem(int big, int medium, int small) {
-        this.big = big;
-        this.medium = medium;
-        this.small = small;
-    }
-    
-    public boolean addCar(int carType) {
-        switch(carType) {
-            case 1: if (big > 0 && big < 1000) { big--;return true;} else {return false;}
-                
-            case 2:if (medium > 0 && medium < 1000) { medium--; return true;} else {return false;}
-            case 3:if (small > 0 && small < 1000) { small--; return true;} else {return false;}
-                
-        }
-        return false;
-    }
-}
-package DP;
-
 import java.util.*;
 
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
+public class QuickSort {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
+    public static int partition(int[] arr, int l, int h) {
 
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
+        int pivot = arr[l];
+        int i = l;
+        int j = h;
+
+        while (i < j) {
+
+            while (arr[i] <= pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(arr, i, j);
+            }
         }
-        // Applying the recurssive solution
+
+        swap(arr, j, l);
+        
+        return j;
+    }
+
+    public static void sort(int[] arr, int l, int h){
+        if (l < h) {
+            int pivot = partition(arr, l, h);
+
+            sort(arr, l, pivot - 1);
+            sort(arr, pivot + 1, h);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sort(arr, 0, n-1);
+
+        printArray(arr);
     }
 }
-package DP;
-
 import java.util.*;
 
-public class Q1 {
+public class QuickSort {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int partition(int[] arr, int l, int h) {
+
+        int pivot = arr[l];
+        int i = l;
+        int j = h;
+
+        while (i < j) {
+
+            while (arr[i] <= pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, j, l);
+        
+        return j;
+    }
+
+    public static void sort(int[] arr, int l, int h){
+        if (l < h) {
+            int pivot = partition(arr, l, h);
+
+            sort(arr, l, pivot - 1);
+            sort(arr, pivot + 1, h);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
 
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
+        sort(arr, 0, n-1);
 
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
-    }
-}
-package DP;
-
-import java.util.*;
-
-public class Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of types of coins: ");
-        int numberOfCoins = sc.nextInt();
-        int[] coins = new int[numberOfCoins];
-        System.out.println("Enter the coins: ");
-        for (int i = 0; i < numberOfCoins; i++) {
-            coins[i] = sc.nextInt();
-        }
-        System.out.println("Enter the sum: ");
-        int sum = sc.nextInt();
-    }
-
-    public static int findWays(int[] coins,int sum){
-        // Declaring a 2-D array
-        // for storing solution to subproblems:
-        int size = coins.length;
-        int[][] arr = new int[size+1][sum+1];
-
-        // Initialize first column of array to 1
-        // because a sum of 0 can be made
-        // in one possible way: {0}
-        for(int i = 0; i < size + 1; i++){
-            arr[i][0] = 1;
-        }
-        // Applying the recurssive solution
+        printArray(arr);
     }
 }
