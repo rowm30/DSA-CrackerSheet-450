33d1895d231a3aa9e19fa90efa53a419418b0ca4
rowm30 <rowm30@gmail.com>
2021-09-09 20:11:18 +0530

    mayank

---
+++ Practice.java
@@ -0,0 +1,48 @@
+package Fundamentals;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    public static int[] bubble(int[] arr){
+        
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        printArray(bubble(arr));
+
+
+    }
+}
