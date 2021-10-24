e5c56bbbb7b175392a8914292209d649345e7339
rowm30 <rowm30@gmail.com>
2021-10-25 02:14:03 +0530

    mayank

---
+++ Practice.java
@@ -0,0 +1,63 @@
+package BinaryTree;
+
+import java.util.*;
+
+import jdk.nashorn.api.tree.Tree;
+
+public class Q1 {
+    
+
+    public static class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    // print in level order
+    public static void levelOrderTraversal(TreeNode startNode){
+        Queue<TreeNode> queue  = new LinkedList<TreeNode>();
+        queue.add(startNode);
+        while(!queue.isEmpty()){
+            TreeNode tempNode = queue.poll();
+            System.out.print(tempNode.data + " ");
+            if (tempNode.left != null) {
+                queue.add(tempNode.left);
+            }
+            if (tempNode.right != null) {
+                queue.add(tempNode.right);
+            }
+        }
+    }
+
+    public static TreeNode createBinaryTree(){
+        TreeNode rootNode = new TreeNode(40);
+        TreeNode node20 = new TreeNode(20);
+        TreeNode node10 = new TreeNode(10);
+        TreeNode node30 = new TreeNode(30);
+        TreeNode node60 = new TreeNode(60);
+        TreeNode node50 = new TreeNode(50);
+        TreeNode node70 = new TreeNode(70);
+        
+        rootNode.left = node20;
+        rootNode.right = node60;
+
+        node20.left = node10;
+        node20.right = node30;
+
+        node60.left = node50;
+        node60.right = node70;
+
+        return rootNode;
+    }
+    
+    public static void main(String[] args){
+        // Creating a binary tree
+        TreeNode rootNode = createBinaryTree();
+        System.out.println("Level order traversal-");
+        levelOrderTraversal(rootNode);
+    }
+
+}
