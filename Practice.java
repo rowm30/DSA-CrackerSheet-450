a4744d0f7542286a6df61fea4992c793f7f1f23e
rowm30 <rowm30@gmail.com>
2021-12-09 10:12:32 +0530

    kmv

--- Practice.java
+++ Practice.java
@@ -1,92 +1,63 @@
-package Fundamentals;
+package BinaryTree;
 
 import java.util.*;
 
-public class MergeSort{
-
-    public static void merge(int[] arr, int l, int m, int r){
-
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        // Create temp arrays
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        // Copy data to temp array
-        for (int i = 0; i < n1; i++) {
-            L[i] = arr[l + i];
-        }
-        for (int i = 0; i < n2; i++) {
-            R[i] = arr[m + 1 + i];
+public class Q6 {
+    
+    public static void preOrder(Node root){
+        // return if the current node is empty
+        if(root == null){
+            return;
         }
 
-        // Merge the temp arrays
+        // Display the data part of the root(or current node)
+        System.out.print(root.data + " ");
 
-        int i = 0, j = 0;
+        // Traverse the left subtree 
+        preOrder(root.left);
 
-        // Initial index of merged sub array
-        int k = l;
-        while(i < n1 && j < n2){
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
+        // Traverse the right subtree
+        preOrder(root.right);
+    }
+
+    public static Node insert(Node root,int data){
+        if(root == null){
+            return new Node(data);
+        }else{
+            Node cur;
+            if(data <= root.data){
+                cur = insert(root.data);
+                root.left = cur;
+            }else{
+                cur = insert(root.right, data);
+                root.right = cur;
             }
-            k++;
-        }
-
-        // copy remaining elements 
-
-        while(i < n1){
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-    public static void sort(int arr[], int l, int r){
-        if (l < r) {
-            // Find the middle point
-            int m = l + (r-l)/2;
-
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m+1, r);
-
-            // Merge the sorted halves
-            merge(arr,l , m,  r);
-        }
-    }
-
-    public static void printArray(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
+            return root;
         }
     }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        int t = sc.nextInt();
+        Node root = null;
+        while(t-- > 0){
+            int data = sc.nextInt();
+            root = insert(root, data);
         }
-
-        sort(arr, 0, n-1);
-
-        printArray(arr);
-
         sc.close();
+        preOrder(root);
+    }
+
+}
+
+class Node{
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
     }
 }
