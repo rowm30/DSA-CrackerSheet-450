96d76a36d615466ec86b082d823aa03c48c35bf0
rowm30 <rowm30@gmail.com>
2021-12-27 11:48:48 +0530

    mayank

--- OOPS/Q2.java
+++ OOPS/Q2.java
@@ -627,3 +627,93 @@ public static void main(String[] args){
     String str = "test string";
     printDups(str);
 }
+
+public static void main(String[] args){
+    String str = "test string";
+    printDups(str);
+}
+                
+                    public static void main(String[] args){
+                        String str = "test string";
+                        printDups(str);
+                    }
+                public static void main(String[] args){
+                    String str = "test string";
+                    printDups(str);
+                }
+            public static void main(String[] args){
+                String str = "test string";
+                printDups(str);
+            }
+public class Q3 {
+static final int NO_OF_CHARS = 256;
+
+static void fillCharCounts(String str,int[] count){
+    for(int i = 0; i < str.length(); i++){
+        count[str.charAt(i)]++;
+    }
+}
+
+static void printDups(String str){
+    int count[] = new int[NO_OF_CHARS];
+    fillCharCounts(str,count);
+    for(int i = 0; i < NO_OF_CHARS; i++){
+        if(count[i] > i){
+            System.out.println((char)(i)+ ", count = " + count[i]);
+        }
+    }
+}
+
+public static void main(String[] args){
+    String str = "test string";
+    printDups(str);
+}
+
+public static void main(String[] args){
+    String str = "test string";
+    printDups(str);
+}  public static void main(String[] args){
+    String str = "test string";
+    printDups(str);
+}
+                
+                    public static void main(String[] args){
+                        String str = "test string";
+                        printDups(str);
+                    }
+                public static void main(String[] args){
+                    String str = "test string";
+                    printDups(str);
+                }
+            public static void main(String[] args){
+                String str = "test string";
+                printDups(str);
+            }
+public class Q3 {
+static final int NO_OF_CHARS = 256;
+
+static void fillCharCounts(String str,int[] count){
+    for(int i = 0; i < str.length(); i++){
+        count[str.charAt(i)]++;
+    }
+}
+
+static void printDups(String str){
+    int count[] = new int[NO_OF_CHARS];
+    fillCharCounts(str,count);
+    for(int i = 0; i < NO_OF_CHARS; i++){
+        if(count[i] > i){
+            System.out.println((char)(i)+ ", count = " + count[i]);
+        }
+    }
+}
+
+public static void main(String[] args){
+    String str = "test string";
+    printDups(str);
+}
+
+public static void main(String[] args){
+    String str = "test string";
+    printDups(str);
+}
