f7dad2da8ac6791e24bc682439c006bdebd6948b
rowm30 <rowm30@gmail.com>
2022-02-05 18:57:56 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -287,7 +287,40 @@ public class Q1 {
             root = new Node(key);
             return root;
         }
+    }package LinkedList;
+
+    import java.util.Scanner;
+    
+    public class Q1 {
+        
+        class Node{
+            int key;
+            Node left,right;
+    
+            Node(int item){
+                key = item;
+                left = right = null;
+            }
+        }
+    
+    
+        // Root of BST
+    
+        Node root;
+    
+        // Constructor
+        Q1(){
+            root = null;
+        }
+    
+        Node insertRec(Node root,int key){
+            if (root == null) {
+                root = new Node(key);
+                return root;
+            }
+        }
     }
+    
 }
 
 
