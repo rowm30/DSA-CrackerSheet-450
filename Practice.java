56a061a1ecd077f8891bcdfc2b3506400bc9d316
rowm30 <rowm30@gmail.com>
2022-02-08 19:00:21 +0530

    mayank

--- Practice.java
+++ Practice.java
@@ -1,401 +1,102 @@
-package LinkedList;
+package DP;
 
-import java.util.Scanner;
+// 0-1 KnapSack
 
-public class Q1 {
+public class Q2 {
     
-    class Node{
-        int key;
-        Node left,right;
+    static int max(int a, int b){
+        return (a > b) ? a : b;
+    }
 
-        Node(int item){
-            key = item;
-            left = right = null;
+    static int KS(int w, int wt[], int val[], int n){
+        // Base Condition
+
+        if(n == 0 || w == 0){
+            return 0;
+        }
+
+        if (wt[n - 1] > w){
+            return KS(w, wt , val, n-1);
+        }else{
+            return max((val[n - 1] + KS(w - wt[n - 1], wt, val, n - 1)), (KS(w, wt, val, n - 1)));
         }
     }
 
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
+    // Driver Code
+    public static void main(String[] args){
+        int val[] = new int[] {60, 100, 120};
+        int wt[] = new int[] {10, 20, 30};
+        int w = 50;
+        int n = val.length;
+        System.out.println(KS(w, wt, val, n));
     }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }
-}
-package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-    package LinkedList;
-
-    import java.util.Scanner;
-    
-    public class Q1 {
-        
-        class Node{
-            int key;
-            Node left,right;
-    
-            Node(int item){
-                key = item;
-                left = right = null;
-            }
-        }
-    
-    
-        // Root of BST
-    
-        Node root;
-    
-        // Constructor
-        Q1(){
-            root = null;
-        }
-    
-        Node insertRec(Node root,int key){
-            if (root == null) {
-                root = new Node(key);
-                return root;
-            }
-        }
-    }
-    
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }
-}
-package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }package LinkedList;
-
-    import java.util.Scanner;
-    
-    public class Q1 {
-        
-        class Node{
-            int key;
-            Node left,right;
-    
-            Node(int item){
-                key = item;
-                left = right = null;
-            }
-        }
-    
-    
-        // Root of BST
-    
-        Node root;
-    
-        // Constructor
-        Q1(){
-            root = null;
-        }
-    
-        Node insertRec(Node root,int key){
-            if (root == null) {
-                root = new Node(key);
-                return root;
-            
-                package LinkedList;
-
-                import java.util.Scanner;
-                
-                public class Q1 {
-                    
-                    class Node{
-                        int key;
-                        Node left,right;
-                
-                        Node(int item){
-                            key = item;
-                            left = right = null;
-                        }
-                    }
-                
-                
-                    // Root of BST
-                
-                    Node root;
-                
-                    // Constructor
-                    Q1(){
-                        root = null;
-                    }
-                
-                    Node insertRec(Node root,int key){
-                        if (root == null) {
-                            root = new Node(key);
-                            return root;
-                        }
-                    }
-                    package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }
-}package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }
-}
-package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }package LinkedList;
-
-    import java.util.Scanner;
-    
-    public class Q1 {
-        
-        class Node{
-            int key;
-            Node left,right;
-    
-            Node(int item){
-                key = item;
-                left = right = null;
-            }
-        }
-    
-    
-        // Root of BST
-    
-        Node root;
-    
-        // Constructor
-        Q1(){
-            root = null;
-        }
-    
-        Node insertRec(Node root,int key){
-            if (root == null) {
-                root = new Node(key);
-                return root;
-            }
-        }
-    }'
-    package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }
-}
-'
- 
-package LinkedList;
-
-import java.util.Scanner;
-
-public class Q1 {
-    
-    class Node{
-        int key;
-        Node left,right;
-
-        Node(int item){
-            key = item;
-            left = right = null;
-        }
-    }
-
-
-    // Root of BST
-
-    Node root;
-
-    // Constructor
-    Q1(){
-        root = null;
-    }
-
-    Node insertRec(Node root,int key){
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-    }
-}
 
 }
+package DP;
 
+// 0-1 KnapSack
 
+public class Q2 {
+    
+    static int max(int a, int b){
+        return (a > b) ? a : b;
+    }
 
-                }
-                }
+    static int KS(int w, int wt[], int val[], int n){
+        // Base Condition
+
+        if(n == 0 || w == 0){
+            return 0;
+        }
+
+        if (wt[n - 1] > w){
+            return KS(w, wt , val, n-1);
+        }else{
+            return max((val[n - 1] + KS(w - wt[n - 1], wt, val, n - 1)), (KS(w, wt, val, n - 1)));
         }
     }
+
+    // Driver Code
+    public static void main(String[] args){
+        int val[] = new int[] {60, 100, 120};
+        int wt[] = new int[] {10, 20, 30};
+        int w = 50;
+        int n = val.length;
+        System.out.println(KS(w, wt, val, n));
+    }
+
+}
+package DP;
+
+// 0-1 KnapSack
+
+public class Q2 {
     
+    static int max(int a, int b){
+        return (a > b) ? a : b;
+    }
+
+    static int KS(int w, int wt[], int val[], int n){
+        // Base Condition
+
+        if(n == 0 || w == 0){
+            return 0;
+        }
+
+        if (wt[n - 1] > w){
+            return KS(w, wt , val, n-1);
+        }else{
+            return max((val[n - 1] + KS(w - wt[n - 1], wt, val, n - 1)), (KS(w, wt, val, n - 1)));
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args){
+        int val[] = new int[] {60, 100, 120};
+        int wt[] = new int[] {10, 20, 30};
+        int w = 50;
+        int n = val.length;
+        System.out.println(KS(w, wt, val, n));
+    }
+
 }
